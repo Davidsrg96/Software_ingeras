743c8a9fc047d6e29e0dd40147afc5fc75879cb2
Danirak <46169406+Danirak@users.noreply.github.com>
2020-11-17 19:44:29 -0300

    Update acciones.blade.php

--- resources/views/layouts/acciones.blade.php
+++ resources/views/layouts/acciones.blade.php
@@ -4,11 +4,11 @@
 <br><script src = "http://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" defer ></script>
 
 <!-- bootstrap -->
-<script src="{{ asset('/componentes/bootstrap/js/bootstrap.min.js') }}"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-autocomplete/1.3.5/jquery.easy-autocomplete.js"></script>
 
+<script src="{{ asset('/componentes/bootstrap/js/bootstrap.min.js') }}"></script>
 
 
 <script src="{{ asset('/componentes/toastr/js/toastr.min.js') }}"></script>
