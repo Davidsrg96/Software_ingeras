c1f4c69438192a3114124f1698da06b2b1a13886
Danirak <46169406+Danirak@users.noreply.github.com>
2020-11-17 21:59:06 -0300

    mejora header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,53 +1,66 @@
-<nav class="navbar navbar-expand-lg ">
-    <div class="collapse navbar-collapse" >
-        <ul class="navbar-nav mr-auto header">
+<nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main_nav">
+        <ul class="navbar-nav">
+            <li class="nav-header">
+                <a class="navbar-brand font-weight-bold" href="{{ route('home.index') }}">Ingeras</a>
+            </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Departamento</a>
-                <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                </div>
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Administracion</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Usuario &raquo </a>
+                     <ul class="submenu dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('usuarios.index') }}">
+                            Usuarios
+                        </a></li>
+                        <li><a class="dropdown-item" href="">Tipos de Usuarios</a></li>
+                        <li><a class="dropdown-item" href="{{ route('cargos.index') }}">
+                            Cargos
+                        </a></li>
+                     </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Departamento &raquo </a>
+                     <ul class="submenu dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('departamentos.index') }}">
+                            Departamentos
+                        </a></li>
+                        <li><a class="dropdown-item" href="">
+                            Actividades
+                        </a></li>
+                     </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Almacenamiento &raquo </a>
+                     <ul class="submenu dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('almacenamiento.index') }}">
+                            Almacenes
+                        </a></li>
+                        <li><a class="dropdown-item" href="{{ route('bodega.index') }}">
+                            Bodegas
+                        </a></li>
+                     </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="{{ route('proveedores.index') }}">
+                      Proveedores
+                  </a>
+                  <li><a class="dropdown-item" href="{{ route('trabajadores.index') }}">
+                      Trabajadores
+                  </a></li>
+                </ul>
             </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('departamentos.index') }}">Departamentos</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="#">Administracion</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('usuarios.index') }}">Usuarios</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('proyectos.index') }}">Proyectos</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('solicitudes.index') }}">Solicitudes</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('almacenamiento.index') }}" > Almacenes</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('proveedores.index') }}">Proveedores</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('bodega.index') }}" >Bodega</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('preguntas.index') }}" >Preguntas</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('tipo_usuario.index') }}" >Tipos de Usuarios</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('cargos.index') }}" >Cargos</a>
-            </li>
-            <li class="nav-item header">
-                <a class="nav-link" href="{{ route('trabajadores.index') }}" >Trabajadores</a>
-            </li>
-            <li class="nav-item header">
+            <li class="nav-item"><a class="nav-link" href="{{ route('proyectos.index') }}">
+                Proyectos
+            </a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('solicitudes.index') }}">
+                Solicitudes
+            </a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('preguntas.index') }}">
+                Preguntas
+            </a></li>
+            <li class="nav-item">
                 <a class="nav-link" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
                 </a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
